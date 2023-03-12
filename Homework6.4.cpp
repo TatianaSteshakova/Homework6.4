d179d7a9abfac1a16a6c1adab8c206309ac9c324
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:21:02 +0300

    Add files via upload

---
+++ Homework6.4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+
+    char c[100];
+
+    cout << "Введите слово: " << endl;
+    cin >> c;
+
+    for (int i = 1; i < strlen(c); i++)
+    {
+        if (c[i] != c[0])
+        {
+            printf("%c", c[i]);
+        }
+    }
+
+    cout << endl;
+
+    // Можно сделать и без буферной строки
+    for (int i = strlen(c)-1; i >= 0; i--)
+    {
+        printf("%c", c[i]);
+    }
+
+
+}
